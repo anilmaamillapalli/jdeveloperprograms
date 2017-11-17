5acb44de669e07485187ca5c9254417f823f78fa
makiam <makiam@makiam-PC>
2017-11-16 22:31:41 -0600

    anil changed saved

--- JavaProject/src/com/makiam/java/FirstJavaProject.java
+++ JavaProject/src/com/makiam/java/FirstJavaProject.java
@@ -2,7 +2,7 @@ package com.makiam.java;
 
 public class FirstJavaProject {
     public static void main(String[] args){
-        System.out.println("This is my first project in GIT");
+        System.out.println("This is my first project in GIT" +"hello");
     
     }
 }
