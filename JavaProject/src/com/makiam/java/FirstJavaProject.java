1f63b8411ec0d444882d7fc8379b1bbe28e13354
makiam <makiam@makiam-PC>
2017-11-16 22:33:41 -0600

    anil changed added

--- JavaProject/src/com/makiam/java/FirstJavaProject.java
+++ JavaProject/src/com/makiam/java/FirstJavaProject.java
@@ -3,6 +3,7 @@ package com.makiam.java;
 public class FirstJavaProject {
     public static void main(String[] args){
         System.out.println("This is my first project in GIT" +"hello");
+        System.out.println("Hello world I am anil kumar");
     
     }
 }
